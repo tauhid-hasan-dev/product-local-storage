6f6e44f00eb6d1767b041b5fa835482cce9173f1
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-09-10 18:32:15 +0300

    without reload project is done

--- js/script.js
+++ js/script.js
@@ -1,24 +1,67 @@
-function getInputValueById(id){
-   const inputField = document.getElementById(id);
-   const inputValue = inputField.value;
-   return inputValue;
+const getInputValueById = id =>{
+    const inputField = document.getElementById(id);
+    const inputValue = inputField.value;
+    inputField.value = '';
+    return inputValue;
 }
 
-function addProduct(){
+const addProduct = () =>{
     const productName = getInputValueById('product-name');
     const productQuantityString = getInputValueById('product-quantity');
     const productQuantity = Number(productQuantityString);
-    /* console.log(productName);
-    console.log(typeof productName); */
-    if(typeof productName !== 'string' || !isNaN(productName) || !Number.isInteger(productQuantity)){
+    //handling error
+    if(!isNaN(productName) || !Number.isInteger(productQuantity)){
        alert('Product name should be written by letters and Product quantity should be a number');
        return
     }
-    console.log(productName);
-    console.log(productQuantity);
-
+   
+    setItemsToLocalStorage(productName, productQuantity);
+    displayProductToUI();
     
-    
-
-
 }
+
+const getItemsFromLocalStorage = ()=>{
+    const products = localStorage.getItem('All-Products');
+    const parseProducts = JSON.parse(products);
+    return parseProducts;
+}
+
+const setItemsToLocalStorage = (name, quantity) => {
+    let products = getItemsFromLocalStorage();
+
+    if(!products){
+        products = [];
+    }
+
+    let obj = {}
+    obj[name] = quantity;
+    products.push(obj)
+    localStorage.setItem('All-Products', JSON.stringify(products));  
+}
+
+const allProducts = document.getElementById('all-products');
+
+const displayProductToUI = () =>{
+    allProducts.textContent = '';
+    let products = getItemsFromLocalStorage();
+    products.forEach(product => {
+         for(const property in product){
+            const key = property;
+            const value = product[property]
+            console.log(key, value);
+            console.log(allProducts);
+            let productCard = document.createElement('div');
+            productCard.innerHTML = `
+            <div class="shadow-sm p-3 mb-2 bg-body rounded">
+                <span class="fs-1">${key}</span>
+                Quantity:<small class="fw-bold">
+                    ${value}
+                </small>
+            </div>
+            `
+            allProducts.appendChild(productCard)
+         }
+    });
+}
+
+displayProductToUI()
